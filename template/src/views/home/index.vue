bb9386147e4ccb81bd83e708af0d0edbf2635a14
chenzhigang <979703986@qq.com>
2023-04-07 17:00:26 +0800

    chore: cascade

--- template/src/views/home/index.vue
+++ template/src/views/home/index.vue
@@ -1,11 +1,21 @@
 <template>
   <hor-view>
     <hor-icon name="hor-version" />
-    <hor-cell label="你好" value="hello world"></hor-cell>
+    <hor-cell @click="handleTest" label="你好" value="hello world"></hor-cell>
+    <hor-cascade-popup ref="horCascadePopupInstance" />
+    <p>11</p>
   </hor-view>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { HorCascadePopupInstance } from '@daysnap/horn-ui'
+
+const horCascadePopupInstance = ref<HorCascadePopupInstance>()
+const handleTest = () => {
+  console.log(1)
+  horCascadePopupInstance.value?.show({})
+}
+</script>
 
 <style lang="scss" scoped>
 // @import '@/assets/scss/define.scss';
